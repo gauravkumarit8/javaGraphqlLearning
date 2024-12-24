d9480d71bc0533079095800eeb396ef0f624f264
gauravkumarit8 <gaurav.kumarit8@gmail.com>
2024-12-24 15:59:33 +0530

    Added the functtionality to check the operation name

--- src/main/java/com/graphql/learning/bookDetails/BookController.java
+++ src/main/java/com/graphql/learning/bookDetails/BookController.java
@@ -1,5 +1,6 @@
 package com.graphql.learning.bookDetails;
 
+import graphql.schema.DataFetchingEnvironment;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
@@ -8,8 +9,14 @@ import org.springframework.stereotype.Controller;
 @Controller
 class BookController {
 
+    //DatafetchingEnvironment is use to getting the other part of the request body query.
     @QueryMapping
-    public Book bookById(@Argument String id) {
+    public Book bookById(@Argument String id, DataFetchingEnvironment environment) {
+        String operationName= environment.getOperationDefinition().getName();
+        //only allow if the operation name is bookDetails
+        if(!"bookDetails".equals(operationName)){
+            throw new RuntimeException("Permission denied..");
+        }
         return Book.getById(id);
     }
 
